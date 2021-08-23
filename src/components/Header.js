5bcb17db0c2a84a9cd676375e3b92f4646053fc6
Harry-Byldd <harinder@byldd.com>
2021-08-23 18:56:22 +0530

    dropdown color changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <Navbar.Brand href="/"><img src="https://res.cloudinary.com/dvc56ayit/image/upload/v1629221759/logo-min_ribx0h.png" className="logo-header"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="m-auto">
+          <Nav className="m-auto header-menu">
             <Nav.Link href="/" className="nav-links">HOME</Nav.Link>
             <Nav.Link href="about_us" className="nav-links">ABOUT US</Nav.Link>
             <NavDropdown title="PROJECTS" id="collasible-nav-dropdown" className="nav-links">
@@ -18,6 +18,7 @@ const Header = () => {
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/projects/never_ending">NEVER ENDING</NavDropdown.Item>
+              <NavDropdown.Divider />
             </NavDropdown>
             <Nav.Link href="/contact_us" className="nav-links">CONTACT</Nav.Link>
             <Nav.Link href="/join_us" className="nav-links">
